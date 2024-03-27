adc010d556bd71ff006ef9a6654bb40f01a98ae9
Muni Sandeep Kumar Ravilla <ravillamunisandeepkumar@gmail.com>
2024-03-26 20:28:35 -0700

    changed about me

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -6,36 +6,10 @@ function AboutCard() {
   return (
     <Card className="quote-card-view">
       <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Muni SandeepKumar Ravilla  </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />
-            I am currently employed as a software developer at Juspay.
-            <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
-            <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
-          </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Games
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Travelling
-            </li>
-          </ul>
-
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
-        </blockquote>
+        <p>
+          Experienced Data Analyst skilled in quantitative analysis, with a Master's in Information Technology from ASU and a Bachelor's in Computer Science from Anna University. At ASU, I led courses in Analyzing Big Data & Data Visualization for IT, engaging over 200 students. At Tata Consultancy Services, I optimized ETL pipelines and implemented Docker/Kubernetes for system deployments, reducing processing time by 50%. My projects include predictive analysis, sentiment analysis, and interactive dashboards, showcasing my ability to leverage data for actionable insights. Proficient in Python, Java, Tableau, and more, I'm excited to drive solutions optimization at your company.
+        </p>
+        <footer className="blockquote-footer">Muni Sandeep Kumar Ravilla</footer>
       </Card.Body>
     </Card>
   );
