da945bcabf69ecf518f1ec1b06f66e3747ebd926
muniravilla <ravillamunisandeepkumar@gmail.com>
2024-08-09 10:12:59 -0700

    Photo changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.svg";
-// import homeLogo from "../../Assets/Sandeep.png";
+// import homeLogo from "../../Assets/home-main.svg";
+import homeLogo from "../../Assets/Profile.png";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
@@ -38,7 +38,7 @@ function Home() {
                 src={homeLogo}
                 alt="home pic"
                 className="img-fluid"
-                style={{ maxHeight: "450px" }}
+                style={{ maxHeight: "1000px",  marginTop: -50  }}
               />
             </Col>
 
